e58f01d8eda81c9ab6697c585e3212b6a3936239
Anastasiya Karpovich <nastasya.karpovich@gmail.com>
2022-07-14 16:08:21 +0300

    add new HttpExampleTest class and add two testmethod

---
+++ src/test/java/HttpExampleTest.java
@@ -0,0 +1,45 @@
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.HttpClients;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.IOException;
+
+public class HttpExampleTest {
+    @Test
+     public void checksStusCodeGetVerbTest(){
+        HttpClient httpClient = HttpClients.createDefault();
+        HttpGet httpGet = new HttpGet("https://www.onliner.by/");
+        try {
+            HttpResponse httpResponse = httpClient.execute(httpGet);
+           int expected = 200;
+            int actual = httpResponse.getStatusLine().getStatusCode();
+            Assert.assertEquals(expected,actual);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    @Test
+    public void checksStusCodePostVerbTest(){
+        HttpClient httpClient = HttpClients.createDefault();
+        HttpPost httpPost = new HttpPost("https://www.onliner.by/");
+        httpPost.setHeader("qwe","qqew");
+        try {
+            HttpResponse httpResponse = httpClient.execute(httpPost);
+            int expected = 200;
+            int actual = httpResponse.getStatusLine().getStatusCode();
+            Assert.assertEquals(expected,actual);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+
+
+}
